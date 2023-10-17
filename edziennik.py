6ae8e30a9c70462fdbdf8b6342751cadb2967e01
ckvg1 <lukasikremigiusz@gmail.com>
2023-10-17 19:50:36 +0200

    pobieranie tematu sprawdzianu

--- edziennik.py
+++ edziennik.py
@@ -1,6 +1,7 @@
 from vulcan import *
 import asyncio
 
+import vulcan
 
 #wczytywanie danych o koncie ze wczesniej zapisanego pliku json. W cudzyslowach wpisz swoja sciezke do pliku
 with open(r"C:\Users\lukas\Desktop\VULCAN API\account.json") as f:
@@ -9,13 +10,37 @@ with open(r"C:\Users\lukas\Desktop\VULCAN API\account.json") as f:
 with open(r"C:\Users\lukas\Desktop\VULCAN API\keystore.json") as f:
     keystore = Keystore.load(f.read())
 
-
 async def main():
     client = Vulcan(keystore, account)
     await client.select_student()
+    
+    n = 10
+    topics_list = [None] * n
 
-    print(client.student)
+    exam = await client.data.get_exams()
+    i = 0
+    async for ex_info in exam:
+        exam_topic = str(ex_info.topic)
+        topics_list[i]=exam_topic
+        print(exam_topic)
+        if(i==n-1):
+            exit(1)
+        else:
+            i+1
+
+
+        
+
+    #exam = [ex_info async for ex_info in exam]
+    #print(exam[0])
+    #for ex_info in exam:
+    #    print(ex_info.topic)
+
+    
+    await client.close()
+    
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+

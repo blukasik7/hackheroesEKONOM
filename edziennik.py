6d145bd2821b9360e2959f2baf12ee2dcd40f1c7
ckvg1 <lukasikremigiusz@gmail.com>
2023-10-17 20:48:20 +0200

    Wyswietlanie tylko przyszlych sprawdzianow

--- edziennik.py
+++ edziennik.py
@@ -1,8 +1,8 @@
 from vulcan import *
 import asyncio
-
 import vulcan
-
+from datetime import datetime
+import os
 #wczytywanie danych o koncie ze wczesniej zapisanego pliku json. W cudzyslowach wpisz swoja sciezke do pliku
 with open(r"C:\Users\lukas\Desktop\VULCAN API\account.json") as f:
     account = Account.load(f.read())
@@ -10,26 +10,30 @@ with open(r"C:\Users\lukas\Desktop\VULCAN API\account.json") as f:
 with open(r"C:\Users\lukas\Desktop\VULCAN API\keystore.json") as f:
     keystore = Keystore.load(f.read())
 
+    
 async def main():
+    os.system('cls')
     client = Vulcan(keystore, account)
     await client.select_student()
     
-    n = 10
-    topics_list = [None] * n
-
+    #ustala obecną date
+    present = datetime.now()
+    
     exam = await client.data.get_exams()
-    i = 0
-    async for ex_info in exam:
-        exam_topic = str(ex_info.topic)
-        topics_list[i]=exam_topic
-        print(exam_topic)
-        if(i==n-1):
-            exit(1)
-        else:
-            i+1
-
-
         
+    async for ex_info in exam:
+        #kod odpowiedzialny za dodawanie tylko tych sprawdzianow, ktorych jeszcze nie bylo
+        str_date_deadline = str(ex_info.deadline)
+        dtt_date_deadline = datetime.strptime(str_date_deadline, '%Y-%m-%d %H:%M:%S')
+        
+        if(present<=dtt_date_deadline):
+            exam_topic = str(ex_info.topic)
+            print(exam_topic)
+            
+        
+        
+
+    
 
     #exam = [ex_info async for ex_info in exam]
     #print(exam[0])

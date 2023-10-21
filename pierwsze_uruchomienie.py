f4c49406768e424def8979a85e7e885cd2497033
blukasik7 <niszczyciel5000pl@gmail.com>
2023-10-21 09:20:36 +0200

    Dodanie logowania przez plik txt

--- pierwsze_uruchomienie.py
+++ pierwsze_uruchomienie.py
@@ -3,32 +3,37 @@ from vulcan import Account
 from vulcan import Vulcan
 import json
 import asyncio
+import os
+
 
 async def main():
-
-    #próbuje cos zrobic, niewazne
-    f = open("test.txt", "r")
+    os.system('cls')
+    # próbuje cos zrobic, niewazne
+    login_list = []
+    f = open("uonet_pass.txt", "r")
     for line in f:
-        print(line, end="")
-
-
-
-    #tworzenie klucza (przez dostep zdalny na dzienniku UONET)
+        login_line = str(line)
+        removed_suffix = login_line.removesuffix("\n")
+        if removed_suffix != ' ':
+            login_list.append(removed_suffix)
+        # print(line, end="")
+    # print(login_list)
+    # tworzenie klucza (przez dostep zdalny na dzienniku UONET)
     keystore = await Keystore.create(device_model="Vulcan API")
-    #zmienne przepisane z panelu Dostep Zdalny w dzienniku UONET
-    token = '3S1KFFD'
-    symbol = 'opole'
-    pin = '355482'
-    #rejestrowanie konta(urządzenia) w dzienniku UONET !!!TY WPISZ SWOJE!!!
+    # zmienne przepisane z panelu Dostep Zdalny w dzienniku UONET
+    token = login_list[0]
+    symbol = login_list[1]
+    pin = login_list[2]
+    # rejestrowanie konta(urządzenia) w dzienniku UONET !!!TY WPISZ SWOJE!!!
     account = await Account.register(keystore, token, symbol, pin)
-    #zapisywanie klucza w pliku json 
+    # zapisywanie klucza w pliku json
     with open("keystore.json", "w") as f:
         f.write(keystore.as_json)
-    #zapisywanie informacji o koncie w pliku json 
+    # zapisywanie informacji o koncie w pliku json
     with open("account.json", "w") as f:
         f.write(account.as_json)
 
-#wykonanie funkcji
+# wykonanie funkcji
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())

d34d6b02842c5c785bdc3bc41c78502dbd2f5606
blukasik7 <niszczyciel5000pl@gmail.com>
2023-11-04 13:28:15 +0100

    powiadomienia

--- edziennik.py
+++ edziennik.py
@@ -7,6 +7,8 @@ import timedelta
 import g4f
 import os.path
 import re
+import time
+from winotify import Notification, audio
 # wczytywanie danych o koncie ze wczesniej zapisanego pliku json. W cudzyslowach wpisz swoja sciezke do pliku
 with open(r"C:\account.json") as f:
     account = Account.load(f.read())
@@ -49,6 +51,7 @@ def generate_objects(list):
         obj_sum += str(obj)
     return str(obj_sum)
 
+
 def generate_topic_paragraph(list):
     p_ids = [
         "pierwszy",
@@ -59,22 +62,27 @@ def generate_topic_paragraph(list):
         "szosty",
         "siodmy",
         "osmy",
-      ];
-    paragraphs =""
+    ]
+    paragraphs = ""
     for i in range(len(list)):
         topic = list[i]
-        paragraph = '''<p style="display:none;" id="'''+p_ids[i]+'''">'''+str(topic)+'''</p>'''
-        paragraphs+=paragraph
+        paragraph = '''<p style="display:none;" id="''' + \
+            p_ids[i]+'''">'''+str(topic)+'''</p>'''
+        paragraphs += paragraph
     return str(paragraphs)
 
+
 def generate_exam_subject(list):
-    sub_ids = ["first", "second","third","fourth","fifth","sixth","seventh","eighth"]
+    sub_ids = ["first", "second", "third", "fourth",
+               "fifth", "sixth", "seventh", "eighth"]
     subjects = ""
     for i in range(len(list)):
         subject = list[i]
-        subject_text = '''<h1 style="margin-top:90px;color:white;display:none;" id="'''+sub_ids[i]+'''">'''+str(subject)+'''</h1>'''
-        subjects +=subject_text
+        subject_text = '''<h1 style="margin-top:90px;color:white;display:none;" id="''' + \
+            sub_ids[i]+'''">'''+str(subject)+'''</h1>'''
+        subjects += subject_text
     return str(subjects)
+
 
 async def main():
     # czysci terminal za kazdym razem
@@ -86,27 +94,28 @@ async def main():
     student = await client.select_student()
     # print(client.student.unit)
     # ustala daty
+
     present = datetime.datetime.today()
     yesterday = present - datetime.timedelta(days=1)
     twodago = present - datetime.timedelta(days=2)
 
     lucky_number = await client.data.get_lucky_number(present)
-    if(lucky_number.number != 0):
-        print("Dzisiejszy szczęśliwy numerek to: " + str(lucky_number.number)+"\n")
-    
+    if (lucky_number.number != 0):
+        print("Dzisiejszy szczęśliwy numerek to: " +
+              str(lucky_number.number)+"\n")
+
     else:
-    
+
         print("Dzisiaj nie ma szczęśliwego numerka \n")
-    
+
     lessons_topics_list = []
 
     # attendance = await client.data.get_attendance();
     # async for attend in attendance:
     #    print(attend.topic)
-    #lessons = await client.data.get_lessons(date_from=yesterday, date_to=yesterday)
-    #async for lesson in lessons:
+    # lessons = await client.data.get_lessons(date_from=yesterday, date_to=yesterday)
+    # async for lesson in lessons:
     #    print(lesson.subject)
-
 
     name = client.student.full_name
     print(name)
@@ -119,17 +128,21 @@ async def main():
     global exam_list
     exam_list = []
     global exam_subjects
-    exam_subjects=[]
+    exam_subjects = []
+    global exam_deadlines
+    exam_deadlines = []
     async for ex_info in exam:
         # kod odpowiedzialny za dodawanie tylko tych sprawdzianow, ktorych jeszcze nie bylo
         str_exam_deadline = str(ex_info.deadline)
+
         exam_deadline = datetime.datetime.strptime(
             str_exam_deadline, '%Y-%m-%d %H:%M:%S')
 
         if (present <= exam_deadline):
+            exam_deadlines.append(exam_deadline)
             exam_topic = str(ex_info.topic)
-            exam_topic = remove_special(re.sub(r'http\S+','',exam_topic))
-            exam_list.append (str(str.replace(exam_topic,'\n','')))
+            exam_topic = remove_special(re.sub(r'http\S+', '', exam_topic))
+            exam_list.append(str(str.replace(exam_topic, '\n', '')))
             exam_subject = str(ex_info.subject.name)
             exam_subjects.append(exam_subject)
     print(return_all(exam_list))
@@ -191,7 +204,9 @@ if __name__ == "__main__":
             else:
                 print("Notatka już istnieje!")
         with open('website2.html', 'w', encoding="utf-8") as website:
-            website.write('''<!DOCTYPE html> <html> <head> <title>Tytuł strony</title> <meta charset="utf-8" /> <link href="styl.css" type="text/css" rel="stylesheet" /> </head> <body>'''+generate_exam_subject(exam_subjects)+generate_topic_paragraph(exam_list)+''' <br /> <div id="left_arrow"><img src="arrow.png" alt="arrow" /></div> <main>''' +generate_objects(exam_list)+''' </main> <div id="right_arrow"><img src="arrow.png" alt="arrow" /></div> <div id="circles_div"></div> <script> var ilosc_plikow ='''+str(len(exam_list))+'''; var NumberOfCircles = ilosc_plikow; var circles_div = document.getElementById("circles_div"); var currentCard = 0; r_arrow = document.getElementById("right_arrow"); l_arrow = document.getElementById("left_arrow"); r_arrow.addEventListener("click", r_arrow_click, false); l_arrow.addEventListener("click", l_arrow_click, false); function r_arrow_click() { console.log("Nacisnąłeś prawą strzałkę"); if (currentCard + 1 < ilosc_plikow) { var div = document.getElementById("circles_div"); var kolka = div.getElementsByTagName("div"); document.getElementById(sub_ids[currentCard]).style.display = "none"; document.getElementById(sub_ids[currentCard + 1]).style.display = "block"; kolka[currentCard].style.background = "white"; kolka[currentCard + 1].style.background = "black"; document.getElementById(ids[currentCard]).style.display = "none"; document.getElementById(ids[currentCard + 1]).style.display = "block"; for (var i = 0; i < ilosc_plikow + 1; i++) { if ((previous_paragraph = document.getElementById(p_ids[i]))) { previous_paragraph.style.display = "none"; } } var current_paragraph = document.getElementById( p_ids[currentCard + 1] ); current_paragraph.style.display = "block"; currentCard++; } } function l_arrow_click() { console.log("Nacisnąłeś lewą strzałkę"); if (currentCard > 0) { var div = document.getElementById("circles_div"); var kolka = div.getElementsByTagName("div"); document.getElementById(sub_ids[currentCard]).style.display = "none"; document.getElementById(sub_ids[currentCard - 1]).style.display = "block"; kolka[currentCard].style.background = "white"; kolka[currentCard - 1].style.background = "black"; document.getElementById(ids[currentCard]).style.display = "none"; document.getElementById(ids[currentCard - 1]).style.display = "block"; for (var i = 0; i < ilosc_plikow + 1; i++) { if ((previous_paragraph = document.getElementById(p_ids[i]))) { previous_paragraph.style.display = "none"; } } current_paragraph = document.getElementById(p_ids[currentCard - 1]); current_paragraph.style.display = "block"; currentCard--; } } for (let i = 0; i < NumberOfCircles; i++) { const circle = document.createElement("div"); circle.setAttribute("id", "circle" + i.toString() + ""); circles_div.appendChild(circle); circle.addEventListener("click", CircleFunction, false); circle.name = i; } ids = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]; p_ids = [ "pierwszy", "drugi", "trzeci", "czwarty", "piaty", "szosty", "siodmy", "osmy", ]; sub_ids = [ "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", ]; function display_first() { element = document.getElementById(ids[0]); element.style.display = "block"; first_circle = document.getElementById("circle0"); first_circle.style.background = "black"; document.getElementById(p_ids[0]).style.display = "block"; document.getElementById(sub_ids[0]).style.display = "block"; } display_first(); function CircleFunction() { current_paragraph = document.getElementById(p_ids[this.name]); current_paragraph.style.display = "block"; console.log( "Wywolales funkcję: CircleFunction poprzez naciśnięcie kółka z ID: " + this.name ); currentCard = this.name; element = document.getElementById(ids[this.name]); element.style.display = "block"; var sub_name = document.getElementById(sub_ids[this.name]); sub_name.style.display = "block"; var div = document.getElementById("circles_div"); var kolka = div.getElementsByTagName("div"); for (var i = 0; i < kolka.length; i++) { kolka[i].style.background = "white"; } this.style.background = "black"; for (var i = 0; i < ilosc_plikow + 1; i++) { if (i != this.name) { previous = document.getElementById(ids[i]); other_paragraphs = document.getElementById(p_ids[i]); var sub_name = document.getElementById(sub_ids[i]); sub_name.style.display = "none"; other_paragraphs.style.display = "none"; previous.style.display = "none"; } } } </script> </body> </html> ''')
+            website.write('''<!DOCTYPE html> <html> <head> <title>Tytuł strony</title> <meta charset="utf-8" /> <link href="styl.css" type="text/css" rel="stylesheet" /> </head> <body>'''+generate_exam_subject(exam_subjects)+generate_topic_paragraph(exam_list)+''' <br /> <div id="left_arrow"><img src="arrow.png" alt="arrow" /></div> <main>''' + generate_objects(exam_list)+''' </main> <div id="right_arrow"><img src="arrow.png" alt="arrow" /></div> <div id="circles_div"></div> <script> var ilosc_plikow ='''+str(len(exam_list)) +
+                          '''; var NumberOfCircles = ilosc_plikow; var circles_div = document.getElementById("circles_div"); var currentCard = 0; r_arrow = document.getElementById("right_arrow"); l_arrow = document.getElementById("left_arrow"); r_arrow.addEventListener("click", r_arrow_click, false); l_arrow.addEventListener("click", l_arrow_click, false); function r_arrow_click() { console.log("Nacisnąłeś prawą strzałkę"); if (currentCard + 1 < ilosc_plikow) { var div = document.getElementById("circles_div"); var kolka = div.getElementsByTagName("div"); document.getElementById(sub_ids[currentCard]).style.display = "none"; document.getElementById(sub_ids[currentCard + 1]).style.display = "block"; kolka[currentCard].style.background = "white"; kolka[currentCard + 1].style.background = "black"; document.getElementById(ids[currentCard]).style.display = "none"; document.getElementById(ids[currentCard + 1]).style.display = "block"; for (var i = 0; i < ilosc_plikow + 1; i++) { if ((previous_paragraph = document.getElementById(p_ids[i]))) { previous_paragraph.style.display = "none"; } } var current_paragraph = document.getElementById( p_ids[currentCard + 1] ); current_paragraph.style.display = "block"; currentCard++; } } function l_arrow_click() { console.log("Nacisnąłeś lewą strzałkę"); if (currentCard > 0) { var div = document.getElementById("circles_div"); var kolka = div.getElementsByTagName("div"); document.getElementById(sub_ids[currentCard]).style.display = "none"; document.getElementById(sub_ids[currentCard - 1]).style.display = "block"; kolka[currentCard].style.background = "white"; kolka[currentCard - 1].style.background = "black"; document.getElementById(ids[currentCard]).style.display = "none"; document.getElementById(ids[currentCard - 1]).style.display = "block"; for (var i = 0; i < ilosc_plikow + 1; i++) { if ((previous_paragraph = document.getElementById(p_ids[i]))) { previous_paragraph.style.display = "none"; } } current_paragraph = document.getElementById(p_ids[currentCard - 1]); current_paragraph.style.display = "block"; currentCard--; } } for (let i = 0; i < NumberOfCircles; i++) { const circle = document.createElement("div"); circle.setAttribute("id", "circle" + i.toString() + ""); circles_div.appendChild(circle); circle.addEventListener("click", CircleFunction, false); circle.name = i; } ids = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]; p_ids = [ "pierwszy", "drugi", "trzeci", "czwarty", "piaty", "szosty", "siodmy", "osmy", ]; sub_ids = [ "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", ]; function display_first() { element = document.getElementById(ids[0]); element.style.display = "block"; first_circle = document.getElementById("circle0"); first_circle.style.background = "black"; document.getElementById(p_ids[0]).style.display = "block"; document.getElementById(sub_ids[0]).style.display = "block"; } display_first(); function CircleFunction() { current_paragraph = document.getElementById(p_ids[this.name]); current_paragraph.style.display = "block"; console.log( "Wywolales funkcję: CircleFunction poprzez naciśnięcie kółka z ID: " + this.name ); currentCard = this.name; element = document.getElementById(ids[this.name]); element.style.display = "block"; var sub_name = document.getElementById(sub_ids[this.name]); sub_name.style.display = "block"; var div = document.getElementById("circles_div"); var kolka = div.getElementsByTagName("div"); for (var i = 0; i < kolka.length; i++) { kolka[i].style.background = "white"; } this.style.background = "black"; for (var i = 0; i < ilosc_plikow + 1; i++) { if (i != this.name) { previous = document.getElementById(ids[i]); other_paragraphs = document.getElementById(p_ids[i]); var sub_name = document.getElementById(sub_ids[i]); sub_name.style.display = "none"; other_paragraphs.style.display = "none"; previous.style.display = "none"; } } } </script> </body> </html> ''')
+
     async def run_all():
         calls = [
             run_provider(provider) for provider in _providers
@@ -200,4 +215,33 @@ if __name__ == "__main__":
 
     asyncio.run(run_all())
     os.system(f'start website2.html')
-    print('___________________________________koniec__________________________________________________')
+    print(len(exam_deadlines))
+    print(len(exam_subjects))
+    print(len(exam_list))
+
+notifications = True
+
+def show_notifications():
+    if notifications:
+        for i in range(len(exam_deadlines)):
+            dates_difference = exam_deadlines[i] - datetime.datetime.now()
+            
+            toast = Notification(app_id="Nazwa aplikacji",
+                                 title="Zbliża się sprawdzian!",
+                                 msg="Za niedlugo masz sprawdzian z " +
+                                 exam_subjects[i] + " z tematu: " +
+                                 exam_list[i] + ". Wejdź na NAZWA APLIKACJI i sprawdź swoje notatki!",
+                                 duration="short")
+            toast.set_audio(audio.SMS, loop=False)
+            if (dates_difference <= timedelta.Timedelta(days=2)):
+                time.sleep(10)
+                print (str(exam_subjects[i])+' '+str(exam_list[i]))
+                print(dates_difference)
+                toast.show()
+                time.sleep(1800)
+            
+
+show_notifications()
+time.sleep(1000)
+show_notifications()
+print('___________________________________koniec__________________________________________________')
---
+++ notify.py
@@ -0,0 +1,4 @@
+from edziennik import *
+main()
+__name__= "__main__"
+show_notifications()
